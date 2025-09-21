7f22619fa873e5967df4406392b0a578e0e1d0de
SeifRedaMabrouk <redar1676@gmail.com>
2025-09-21 23:20:36 +0300

    Offffffff

---
+++ Smoke_demo_test.py
@@ -0,0 +1,23 @@
+from gpiozero import DigitalInputDevice, PWMOutputDevice
+from time import sleep
+
+smoke_sensor = DigitalInputDevice(26)
+buzzer = PWMOutputDevice(12)
+
+freq = 200
+step = 100
+
+while True:
+    if smoke_sensor.value == 1:
+        print(f"Smoke detected! freq={freq} Hz")
+        buzzer.frequency = freq
+        buzzer.value = 0.9
+        sleep(2)
+        if freq <= 1000:
+            freq += step
+    else:
+        print("No smoke")
+        buzzer.off()
+        freq = 200
+        sleep(0.5)
+
